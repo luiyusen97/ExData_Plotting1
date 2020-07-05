82e5a4bda1576f2135bee67b1271420ea2de35bb
luiyusen97 <luiyusen97@gmail.com>
2020-07-05 16:27:09 +0800

    Subsetted required data. Use this code for all 4 plots.

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+library(lubridate)
 if (!file.exists("rawdata//powerdata.zip")){
     download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
                   destfile = "rawdata//powerdata.zip")
@@ -6,3 +8,12 @@ if (!file.exists("rawdata//powerdata//household_power_consumption.txt")){
     unzip(zipfile = "rawdata//powerdata.zip",
           exdir = "rawdata//powerdata")
 }
+powerdat <- read.table(file = "rawdata//powerdata//household_power_consumption.txt", header = TRUE,
+                       sep = ";", na.strings = "?")
+powerdat[ , 1] <- apply(powerdat[ , 1, drop = F], 2, function(x){
+    gsub(pattern = "/", replacement = "", x = x)
+})
+powerdat[ , 1] <- apply(powerdat[ , 1, drop = F], 2, function(x){
+    as.numeric(x)
+})
+subset_dat <- subset(powerdat, Date == 01022007|Date == 02022007)
